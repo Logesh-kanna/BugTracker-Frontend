91943fe48618d650e85bd307b47ed17ce286ef5b
Logesh-kanna <logeshkanna@macappstudio.com>
2025-02-06 13:22:11 +0530

    adding dict file

---
+++ dist/front-end/assets/js/common.js
@@ -0,0 +1,10 @@
+// function swalFunc(title, text, icon) {
+//   const result = Swal.fire({
+//     title: title,
+//     text: text,
+//     icon: icon,
+//     confirmButtonText: 'OK'
+//   });
+//   console.log(result); // This should log the Promise
+//   return result;
+// }
